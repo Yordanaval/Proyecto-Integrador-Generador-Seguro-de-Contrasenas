b3f632abcff0973b2c21dd877f75f4909139f544
Yordana Cedeño <yordanarodrig@gmail.com>
2025-12-11 22:51:37 -0500

    Subiendo funciones nuevas
    Se añadió el generador de contraseñas y así mismo el evaluador de seguridad que le indicará al usuario si su nivel es: Seguro/Medio/Débil.

--- Tarea.py
+++ Tarea.py
@@ -1,5 +1,8 @@
 #Generador seguro de contraseña
 
+#Se importa la librería secrets para que se pueda elegir caracteres aleatorios.
+import secrets
+
 #Se crean las variables con los caracteres que se van a utilizar al momento de generar la contraseña segura.
 
 letras_minusculas = "abcdefghijklmnñopqrstuvwxyz" 
@@ -41,3 +44,56 @@ while opcion_1 == "no" and opcion_2 == "no" and opcion_3 == "no":
 print("Generando contraseña aleatoria...")
 print("Evaluando nivel de seguridad...")
 
+#Se crea una variable para almacenar la contraseña que se va a generar. Por defecto, es en minúsculas.
+contraseña = letras_minusculas
+
+#Dependiendo de las opciones que elija el usuario, se irán añadiendo los caracteres a la variable de la contraseña.
+if opcion_1 == "si":
+    contraseña += letras_mayusculas
+if opcion_2 == "si":
+    contraseña += numeros
+if opcion_3 == "si":
+    contraseña += simbolos
+
+#Se genera la contraseña con la longitud y los caracteres elegidos por el usuario.
+resultado = ''.join(secrets.choice(contraseña) for i in range(longitud))
+
+#Se evalua la contraseña y se le indica al usuario si es segura o débil.
+#Para esto se crea la variable puntos, que se incrementará en cada una de las opciones elegidas, iniciando por la longitud.
+puntos = 0
+
+if longitud >= 10:
+    puntos += 7
+
+elif longitud >= 6:
+    puntos += 5
+elif longitud >= 4:
+    puntos += 3
+else:
+    puntos += 1
+
+#Las opciones elegidas por el usuario también incrementan los puntos.
+if opcion_1 == "si":
+    puntos += 1
+
+if opcion_2 == "si":
+    puntos += 1
+
+if opcion_3 == "si":
+    puntos += 1
+
+#Dependiendo de los puntos obtenidos, se le asigna un nivel de seguridad a la contraseña.
+nivel_de_seguridad = ""
+if puntos >= 10:
+    nivel_de_seguridad = "Segura"
+elif puntos >= 7:
+    nivel_de_seguridad = "Media"
+elif puntos >= 4:
+    nivel_de_seguridad = "Débil" 
+
+#Se le muestra al usuario la contraseña generada y su nivel de seguridad.
+print("Contraseña generada: " + resultado)
+print("Nivel de seguridad: " + nivel_de_seguridad)
+
+#Se le pregunta al usuario si desea generar otra contraseña.
+continuar = input("¿Desea generar otra contraseña?: ")
