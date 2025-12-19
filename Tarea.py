ad1ed75e665334a956355432f0d902778f4f4daa
Yordana Cedeño <yordanarodrig@gmail.com>
2025-12-19 16:33:25 -0500

    Titulo y salto de línea
    Se puso un titulo al programa y tambien salto de linea entre las salidas para que se entienda mejor al momento de leer.

--- Tarea.py
+++ Tarea.py
@@ -10,41 +10,44 @@ letras_mayusculas = letras_minusculas.upper()
 numeros = "0123456789"
 simbolos = "@!*#$%"
 
+#Titulo del programa.
+print("Generador seguro de contraseñas" "\n")
+
 #Se inicia un ciclo para que el usuario pueda generar varias contraseñas si así lo desea.
 while True:
 
 #Se pregunta al usuario la longitud de caracteres que desea en la contraseña.
 
-    longitud = input("Ingresa la longitud deseada para la contraseña: ")
+    longitud = input("Ingresa la longitud deseada para la contraseña: " "\n")
 
 #Si el usuario escribe palabras, el programa no lo dejará avanzar hasta que escriba un número.
     while not longitud.isdigit():     
-        print("Debe ingresar solo números, no letras")
-        longitud = input("Ingresa la longitud deseada para la contraseña: ")
+        print("Debe ingresar solo números, no letras""\n")
+        longitud = input("Ingresa la longitud deseada para la contraseña: ""\n")
 
 #La cadena de texto se hace número y se le hace saber al usuario que es válida la longitud.
     longitud = int(longitud) 
-    print("Longitud válida")
+    print("Longitud válida""\n")
 
 #Se crea una función para que cuando el usuario elija una respuesta sea solo "si" o "no" y de este modo sea más fácil el código.
     def opciones(respuesta):
         return "si" if respuesta.lower() in ("si", "s", "sí", "y") else "no"
 
-    opcion_1 = opciones(input("¿Desea añadir mayúsculas?: "))
-    opcion_2 = opciones(input("¿Desea añadir números?: "))
-    opcion_3 = opciones(input("¿Desea añadir símbolos?: "))
+    opcion_1 = opciones(input("¿Desea añadir mayúsculas?: ""\n"))
+    opcion_2 = opciones(input("¿Desea añadir números?: ""\n"))
+    opcion_3 = opciones(input("¿Desea añadir símbolos?: ""\n"))
 
 #Si el usuario no elige ninguna opción, el programa no lo dejará avanzar.
     while opcion_1 == "no" and opcion_2 == "no" and opcion_3 == "no":
 
-        opcion_1 = opciones(input("¿Desea añadir mayúsculas?: "))
-        print("Debe elegir al menos una opción")
-        opcion_2 = opciones(input("¿Desea añadir números?: "))
-        opcion_3 = opciones(input("¿Desea añadir símbolos?: "))
+        opcion_1 = opciones(input("¿Desea añadir mayúsculas?: ""\n"))
+        print("Debe elegir al menos una opción""\n")
+        opcion_2 = opciones(input("¿Desea añadir números?: ""\n"))
+        opcion_3 = opciones(input("¿Desea añadir símbolos?: ""\n"))
 
 #Se le hace saber al usuario que la contraseña será creada a partir de lo que elegió y además que se evaluará su nivel de seguridad.
-    print("Generando contraseña aleatoria...")
-    print("Evaluando nivel de seguridad...")
+    print("Generando contraseña aleatoria...""\n")
+    print("Evaluando nivel de seguridad...""\n")
 
 #Se crea una variable para almacenar la contraseña que se va a generar. Por defecto, es en minúsculas.
     contraseña = letras_minusculas
@@ -79,7 +82,7 @@ while True:
     puntos = 0
 
     if longitud >= 10:
-        puntos += 7
+        puntos += 8
 
     elif longitud >= 6:
         puntos += 5
@@ -110,17 +113,17 @@ while True:
         nivel_de_seguridad = "Muy débil"
 
 #Se le muestra al usuario la contraseña generada y su nivel de seguridad.
-    print("Contraseña generada: " + contraseña_final)
-    print("Nivel de seguridad: " + nivel_de_seguridad)
+    print("Contraseña generada: " + contraseña_final, "\n")
+    print("Nivel de seguridad: " + nivel_de_seguridad, "\n")
 
 #Se le pregunta al usuario si desea generar otra contraseña.
     def opciones(respuesta):
         return "si" if respuesta.lower() in ("si", "s", "sí", "y") else "no"
 
-    continuar = opciones(input("¿Desea generar otra contraseña?: "))
+    continuar = opciones(input("¿Desea generar otra contraseña?: ""\n"))
 
     if continuar == "si":
         continue
     else:
-        print("Gracias por usar el generador de contraseñas seguras.")
+        print("Gracias por usar el generador de contraseñas seguras.""\n")
         break
